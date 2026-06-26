98318ebdce8d4eb12960688c426921543e5330c4
alex <admitnon124@gmail.com>
2026-06-26 09:47:53 +0000

    Добавлена дата и проверка

--- df_script.sh
+++ df_script.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-df -h > ~/github_learn/df.txt
+echo "Отчет сгенерирован по пути: ~/github_learn/df.txt"
+
+echo "--> Проверка места на диске <--" > ~/github_learn/df.txt 
+date >> ~/github_learn/df.txt
+echo "---" >> ~/github_learn/df.txt
+df -h >> ~/github_learn/df.txt
+
+if command -v df &> /dev/null; then
+	df -h >> ~/github_learn/df.txt
+else
+	echo "Ошибка: df не найден" >> ~/github_learn/df.txt
+fi
